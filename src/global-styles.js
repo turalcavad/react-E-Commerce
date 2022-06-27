9441c64d11f33d021258f6e2a2c8fce8d84b2c1f
turalcavadn <turaltj@code.edu.az>
2022-06-27 22:19:55 +0400

    created global-style

---
+++ src/global-styles.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  *, *:before, *:after {
+    box-sizing: border-box;
+  }
+  html, body {
+	font-family: "Urbanist", sans-serif;
+}
+a, button, img, input, span, h4 {
+    transition: all 0.3s ease 0s;
+}
+input{
+    border: none;
+    outline: none;
+}
+`;
